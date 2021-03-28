ef9335833b676e4b5b11e57ebe906ee5e8a3451a
sidd-oo <sahoosiddharth3@gmail.com>
2021-03-28 20:38:49 +0530

    Input field styled and Todos are made into lists

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+inputElem = document.querySelector('#inputElem')
+allTasks = document.querySelector('.all ul');
 
-console.log("Hello")
+function createListItemAndAppend(Tasks, listText){
+    const list = document.createElement('li');
+    list.classList.add("listItem")
+    list.textContent = listText
+    Tasks.append(list);
+
+}
+
+inputElem.addEventListener('keypress',(event)=>{
+    if(event.key == "Enter"){
+        if(event.target.value !== ""){
+            //List is created and appended in task tab
+            createListItemAndAppend(allTasks,event.target.value)
+        }
+        //reseting the last value of the input
+        event.target.value = ""
+    }
+})
